033e488dac396b8f5d233a5bbb769156efb7e090
Ravihansa <ravihansapemindu@gmail.com>
2026-07-01 17:31:49 +0530

    football club controll edited

--- src/main/java/com/example/SportsTracker/football/controller/FootballClubController.java
+++ src/main/java/com/example/SportsTracker/football/controller/FootballClubController.java
@@ -13,15 +13,3 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/football/clubs")
 @RequiredArgsConstructor
-public class FootballClubController {
-    private final FootballClubService service;
-
-    @GetMapping
-    public ResponseEntity<List<FootballClub>> getAll() { return ResponseEntity.ok(service.getAll()); }
-
-    @PostMapping
-    public ResponseEntity<FootballClub> create(@Valid @RequestBody ClubRequest req) { return ResponseEntity.ok(service.create(req)); }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<FootballClub> update(@PathVariable String id, @Valid @RequestBody ClubRequest req) { return ResponseEntity.ok(service.update(id, req)); }
-}
